94f696f188afa703b3f0f2b020a68c2572d2d4de
jonathangarro <jonathan.garro@gmail.com>
2025-07-04 09:02:20 -0700

    add airport functionality

--- users/admin.py
+++ users/admin.py
@@ -4,7 +4,7 @@ Admin configuration for the users app.
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
-from .models import Language, Region, UserProfile
+from .models import Language, Region, UserProfile, Airport, LanguageProficiency
 
 
 @admin.register(Language)
@@ -19,19 +19,34 @@ class RegionAdmin(admin.ModelAdmin):
     search_fields = ('name', 'code')
 
 
+@admin.register(Airport)
+class AirportAdmin(admin.ModelAdmin):
+    list_display = ('name', 'city', 'country', 'iata_code')
+    search_fields = ('name', 'city', 'iata_code')
+    list_filter = ('country',)
+
+
+@admin.register(LanguageProficiency)
+class LanguageProficiencyAdmin(admin.ModelAdmin):
+    list_display = ('user_profile', 'language', 'proficiency')
+    list_filter = ('language', 'proficiency')
+    search_fields = ('user_profile__user__username', 'language__name')
+
+
 class UserProfileInline(admin.StackedInline):
     model = UserProfile
     can_delete = False
     verbose_name_plural = 'Profile'
-    filter_horizontal = ('languages', 'preferred_regions', 'restricted_countries', 'qualified_profiles')
+    filter_horizontal = ('preferred_regions', 'restricted_countries', 'qualified_profiles')
 
 
 class UserAdmin(BaseUserAdmin):
     inlines = (UserProfileInline,)
     list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'get_languages')
-    
+
     def get_languages(self, obj):
-        return ", ".join([lang.name for lang in obj.profile.languages.all()])
+        return ", ".join([f"{lang_prof.language.name} ({lang_prof.get_proficiency_display()})" 
+                         for lang_prof in obj.profile.language_proficiencies.all()])
     get_languages.short_description = 'Languages'
 
 
