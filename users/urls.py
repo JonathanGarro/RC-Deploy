097fdc92a14bfe1983a155140d4fd36c1362165e
jonathangarro <jonathan.garro@gmail.com>
2025-07-06 12:01:47 -0700

    add save/del alert

--- users/urls.py
+++ users/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('profile/', views.profile_view, name='profile'),
     path('profile/update/', views.profile_update, name='profile_update'),
     path('airports/search/', views.search_airports, name='search_airports'),
+    path('alerts/save/<int:alert_id>/', views.save_alert, name='save_alert'),
+    path('alerts/remove/<int:alert_id>/', views.remove_alert, name='remove_alert'),
 ]
